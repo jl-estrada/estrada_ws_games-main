bb35db5834000e8d5d27130ff0ed40fd1ce2eff3
jl-estrada <ejuliamay@gmail.com>
2025-11-19 18:19:30 +0800

    part3

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,7 +8,8 @@
             <tr>
                 <th>Username</th>
                 <th>Registered at</th>
-                <th>Last Login</th>
+                <th>Last login</th>
+                <th>Profile</th>
             </tr>
         </thead>
         <tbody>
